eb51370392d753928388563a220e16e3761ea33c
bigbugboy <liuxu-ynl@foxmail.com>
2023-12-15 18:22:08 +0800

    update

--- apps/exercise/views.py
+++ apps/exercise/views.py
@@ -17,10 +17,10 @@ def index(request):
 @method_decorator(login_required, name='post')
 class ScStart(View):
 
-    def _json_to(sc: models.SingleChoice):
+    def _json_to(self, sc: models.SingleChoice):
         return {
             'no': sc.no,
-            'tags': [],     # todo
+            'tags': [tag for tag in sc.tags.all().values()], 
             'star': sc.star,
             'title': sc.title,
             'choice_a': sc.choice_a,
@@ -38,22 +38,26 @@ class ScStart(View):
         todo_sc_no = request.session.get('todo_sc_no')
         if todo_sc_no:
             sc = get_object_or_404(models.SingleChoice, no=todo_sc_no)
+            print(111)
+            print()
             return JsonResponse(self._json_to(sc))
         
         # 新的题目
         scs = models.SingleChoice.objects.filter(star=1, status=2).all()
         if vip:
           scs = models.SingleChoice.objects.filter(star=star, status=2).all()
-        sc = random.sample(scs, 1)
+
+        sc = random.choice(scs)
         request.session['todo_sc_no'] = sc.no
         return JsonResponse(self._json_to(sc))
 
 
     def post(self, request: HttpRequest):
-        todo_sc_no = request.headers['todo_sc_no']
-        right_choice = request.headers['right_choice']
+        todo_sc_no = request.headers['todo-sc-no']
+        right_choice = request.headers['right-choice']
         sc = get_object_or_404(models.SingleChoice, no=todo_sc_no)
-
+        print(sc.right_choice)
+        print(right_choice)
         return JsonResponse({
             'status': right_choice == sc.right_choice,
             'rc': sc.right_choice,
