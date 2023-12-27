a787b5567a69642e5c3377984d8beba77e32aa99
bigbugboy <liuxu-ynl@foxmail.com>
2023-12-27 21:15:20 +0800

    fix bug

--- apps/userinfo/middlewares.py
+++ apps/userinfo/middlewares.py
@@ -5,6 +5,7 @@ from django.shortcuts import redirect
 
 class SuperUserAdminMiddleware(MiddlewareMixin):
     def process_request(self, request: HttpRequest):
-        user = request.user
-        if not user.is_authenticated or not user.is_superuser:
-            return redirect(to='index')
+        if request.path_info.startswith('/admin'):
+            user = request.user
+            if not user.is_authenticated or not user.is_superuser:
+                return redirect(to='index')
